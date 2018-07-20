4853d95df47853f944d7d757a293c872db3db18e
Jonas Gutauskas <jonas.gutauskas.jg@gmail.com>
2018-07-20 16:03:35 +0300

    adds markers on the map based on the fetched locations

--- src/App.js
+++ src/App.js
@@ -1,30 +1,69 @@
 import React, { Component } from 'react';
 import './App.css';
-import GoogleMaps from './MapsApi'
+import GoogleMaps from './MapsApi';
+import Maps from './Map';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.map;
+    this.geocoder;
     this.mapRef = React.createRef();
+    this.location = {lat: 54.897901, lng: 23.892914};
+    this.locPlaces = [];
   }
 
   componentDidMount() {
     GoogleMaps.getMaps.call(this);
   }
 
+/* initilizes the map */
   initMap() {
     this.map = new window.google.maps.Map(this.mapRef.current, {
-           center: {lat: 40.74135, lng: -73.99802},
-           zoom: 14
-        });
+      center: this.location,
+      zoom: 14,
+      mapTypeControl: false,
+    });
+
+    const places = new window.google.maps.places.PlacesService(this.map);
+    places.nearbySearch({
+      location: this.location,
+      radius: 40,
+    }, this.managePlaces.bind(this));
       console.log(999);
   }
+
+/* puts all of the places in an array */
+managePlaces(response, status) {
+  console.log(response, status);
+  if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+    for (let i = 0; i < response.length; i++) {
+      this.locPlaces.push(response[i]);
+    }
+  } else {
+    return;
+    console.log('Places could not be loaded');
+  }
+  this.createMarkers();
+}
+
+/* creates markers */
+createMarkers(place) {
+  this.locPlaces.forEach(loc => {
+    console.log(loc);
+    const markers = new window.google.maps.Marker({
+      map: this.map,
+      position: {lat: loc.geometry.location.lat(), lng: loc.geometry.location.lng()}
+    });
+  });
+}
 
   render() {
     return (
       <div className="App">
-          <div id='map' ref={this.mapRef}></div>
+        <Maps
+          reference={this.mapRef}
+        />
       </div>
     );
   }
